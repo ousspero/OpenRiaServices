eb71017c145d16a6675188eb8ac84ea98dfc8a6f
Pierre Barjon <barjon.pierre@gmail.com>
2022-09-15 11:51:15 +0200

    Update nuspec coryright and assemblies version

--- OpenRiaServices.DomainServices.Client.Web/Framework/Properties/AssemblyInfo.cs
+++ OpenRiaServices.DomainServices.Client.Web/Framework/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
 // AssemblyFileVersion attribute is generated automatically by a custom MSBuild task inside AutomaticAssemblyVersion.targets
 //[assembly: AssemblyFileVersion("1.0.0.14")]
